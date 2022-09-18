0785de69722c88f1f66e9886629d31a7fa8464a7
Hudaifa Abdullah ツ <loffy@live.com>
2022-09-18 14:36:21 +0500

    C# enabler

---
+++ HibernationEnabler/HibernationEnabler/Program.cs
@@ -0,0 +1,25 @@
+﻿if (args.Length > 0)
+{
+    foreach (var arg in args)
+    {
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+        {
+            WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+            FileName = "cmd.exe"
+        };
+
+        if (arg == "enable")
+            startInfo.Arguments = "/C powercfg -H on";
+        else if (arg == "disable")
+            startInfo.Arguments = "/C powercfg -H off";
+
+        process.StartInfo = startInfo;
+        process.Start();
+        Console.WriteLine($"Argument = {arg}");
+    }
+}
+else
+{
+    Console.WriteLine("No arguments");
+}
